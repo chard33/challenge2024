f4bf1134ce8dcd948e76ae73fa9ba2578a5c9a61
Chard33 <jchar33@hotmail.com>
2024-07-13 09:27:46 -0600

    Definicion de funcionalidades

--- index.js
+++ index.js
@@ -4,35 +4,13 @@ let div_sin = document.querySelector(".sin-txt");
 let txt_res = document.querySelector(".tx_res");
 let btn_des = document.querySelector(".btn-dos");
 
-function encriptar(){
-
-    validar("encriptar")
-}
-
-function desencriptar(){
-
-    validar("desencriptar");
-}
-
-function validar(accion){
-    if (txt_ent.value.length === 0) {
-        txt_ent.classList.add("red");
-    } else {
-
-        div_sin.style.display = "none";
-
-        if (accion === 'encriptar') {
-
-            div_res.style.display = "initial";
-            txt_res.value = txt_ent.value;
-            btn_des.disabled = false;
-            
-        } else if (accion === 'desencriptar') {
-            
-            txt_res.value = txt_ent.value;
-        }
-    }
-}
+var arr = {
+    "e": "enter",
+    "i": "imes",
+    "a": "ai",
+    "o": "ober",
+    "u": "ufat"
+};
 
 function copiar(){
 
@@ -48,5 +26,29 @@ function val_txtA(){
     if(window.event.keyCode == 8 && txt_ent.value.length === 0){
         div_sin.style.display = "initial";
         div_res.style.display = "none";
+    }
+}
+
+function procesarTexto(accion) {
+    if (txt_ent.value.length === 0) {
+        txt_ent.classList.add("red");
+    } else {
+        div_sin.style.display = "none";
+        div_res.style.display = "initial";
+        btn_des.disabled = false;
+
+        let cadena = txt_ent.value;
+
+        if (accion === 'encriptar') {
+            for (var llav in arr) {
+                cadena = cadena.replace(new RegExp(llav, "g"), arr[llav]);
+            }
+        } else if (accion === 'desencriptar') {
+            for (var llav in arr) {
+                cadena = cadena.replace(new RegExp(arr[llav], "g"), llav);
+            }
+        }
+
+        txt_res.value = cadena;
     }
 }
